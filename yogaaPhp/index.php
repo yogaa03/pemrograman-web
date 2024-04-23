84119907d2d563fc572690a3af1ec7f1880a3963
yogaa03 <149873753+yogaa03@users.noreply.github.com>
2024-04-23 10:55:37 +0800

    Add files via upload

---
+++ yogaaPhp/index.php
@@ -0,0 +1,71 @@
+<?php 
+    /*
+    session_start();
+    include_once('prak1.php');
+    echo "<hr>";
+    include_once('prak2.php');
+    echo "<hr>";
+    include_once('prak3.php');
+    echo "<hr>";
+    include_once('kondisi1.php');
+    echo "<hr>";
+    include_once('kondisi2.php');
+    echo "<hr>";
+    include_once('kondisi3.php');
+    echo "<hr>";
+    include_once('perulangan1.php');
+    echo "<hr>";
+    include_once('perulangan2.php');
+    echo "<hr>";
+    include_once('perulangan3.php');
+    echo "<hr>";
+    include_once('perulangan4.php');
+    echo "<hr>";
+    include_once('perulangan5.php');
+    echo "<hr>";
+    include_once('perulangan6.php');
+    echo "<hr>";
+    include_once('array1.php');
+    echo "<hr>";
+    include_once('array2.php');
+    echo "<hr>";
+    include_once('array3.php');
+    echo "<hr>";
+    include_once('function1.php');
+    echo "<hr>";
+    include_once('function2.php');
+    echo "<hr>";
+    */
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ardane PHP</title>
+</head>
+<body>
+
+<h1>PRAKTEK PHP - PEMROGRAMAN WEB</h1>
+
+<a href="prak1.php">Praktek 1</a><br>
+<a href="prak2.php">Praktek 2</a><br>
+<a href="prak3.php">Praktek 3</a><br>
+<a href="kondisi1.php">Kondisi 1</a><br>
+<a href="kondisi2.php">Kondisi 2</a><br>
+<a href="kondisi3.php">Kondisi 3</a><br>
+<a href="perulangan1.php">Perulangan 1</a><br>
+<a href="perulangan2.php">Perulangan 2</a><br>
+<a href="perulangan3.php">Perulangan 3</a><br>
+<a href="perulangan4.php">Perulangan 4</a><br>
+<a href="perulangan5.php">Perulangan 5</a><br>
+<a href="perulangan6.php">Perulangan 6</a><br>
+<a href="array1.php">Array 1</a><br>
+<a href="array2.php">Array 2</a><br>
+<a href="array3.php">Array 3</a><br>
+<a href="function1.php">Function 1</a><br>
+<a href="function2.php">Function 2</a><br>
+
+</body>
+</html>
